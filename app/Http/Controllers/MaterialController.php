f9f463a5a93d80489413e51afa12d4fa9652b3ab
ajikamaludin <aji19kamaludin@gmail.com>
2023-03-15 21:10:58 +0700

    crud material

---
+++ app/Http/Controllers/MaterialController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Material;
+use Illuminate\Http\Request;
+
+class MaterialController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Material::query();
+
+        if ($request->q) {
+            $query->where('name', 'like', "%{$request->q}%");
+        }
+
+        $query->orderBy('created_at', 'desc');
+
+        return inertia('Material/Index', [
+            'query' => $query->paginate(10),
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        Material::create([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        session()->flash('message', ['type' => 'success', 'message' => 'Item has beed saved']);
+    }
+
+    public function update(Request $request, Material $material)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+
+        $material->update([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        session()->flash('message', ['type' => 'success', 'message' => 'Item has beed updated']);
+    }
+
+    public function destroy(Material $material)
+    {
+        $material->delete();
+
+        session()->flash('message', ['type' => 'success', 'message' => 'Item has beed deleted']);
+    }
+}
