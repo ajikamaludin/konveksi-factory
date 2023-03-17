5b15b5b93ff1d3481f3a8232db0f710ad1132a2e
ajikamaludin <aji19kamaludin@gmail.com>
2023-03-17 09:04:06 +0700

    add dashboard

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Production;
+use App\Models\ProductionItemResult;
+use App\Models\User;
+
 class GeneralController extends Controller
 {
     public function index()
     {
-        return inertia('Dashboard');
+        $artikel = Production::count();
+        $result = ProductionItemResult::count();
+        $result_today = ProductionItemResult::whereDate('input_at', now())->count();
+        $user = User::count();
+        return inertia('Dashboard', [
+            'artikel' => $artikel,
+            'result' => $result,
+            'result_today' => $result_today,
+            'user' => $user,
+        ]);
     }
 
     public function maintance()
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -3,6 +3,13 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard(props) {
+    const { 
+        artikel,
+        result,
+        result_today,
+        user,
+    } = props
+
     return (
         <AuthenticatedLayout
             auth={props.auth}
@@ -15,8 +22,31 @@ export default function Dashboard(props) {
 
             <div>
                 <div className="mx-auto sm:px-6 lg:px-8 ">
-                    <div className="overflow-hidden shadow-sm sm:rounded-lg bg-white">
-                        <div className="p-6 ">Dashboard</div>
+                    <div className='grid grid-cols-2 md:grid-cols-4 gap-2'>
+                        <div className="overflow-hidden shadow-sm sm:rounded-lg bg-white">
+                            <div className="p-6 ">
+                                <div className='text-lg'>Artikel</div>
+                                <div className='font-bold text-2xl'>{artikel}</div>
+                            </div>
+                        </div>
+                        <div className="overflow-hidden shadow-sm sm:rounded-lg bg-white">
+                            <div className="p-6 ">
+                                <div className='text-lg'>Total Line Sewing</div>
+                                <div className='font-bold text-2xl'>{result}</div>
+                            </div>
+                        </div>
+                        <div className="overflow-hidden shadow-sm sm:rounded-lg bg-white">
+                            <div className="p-6 ">
+                                <div className='text-lg'>Line Sewing Hari Ini</div>
+                                <div className='font-bold text-2xl'>{result_today}</div>
+                            </div>
+                        </div>
+                        <div className="overflow-hidden shadow-sm sm:rounded-lg bg-white">
+                            <div className="p-6 ">
+                                <div className='text-lg'>User</div>
+                                <div className='font-bold text-2xl'>{user}</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
